42da0ac228686a763186feaff880c6336fa25d49
Steve Frost <steve@stevefrost.net>
2025-02-05 13:46:45 +0000

    images issue with Vercel

--- react-whois-lookup/src/assets/components/Navbar.jsx
+++ react-whois-lookup/src/assets/components/Navbar.jsx
@@ -1,9 +1,12 @@
+import logo from "../images/logo.png";
+
 function Navbar() {
   return (
     <div id="navbar" className="navbar">
       <img
         id="logo"
-        src="/src/assets/images/logo.png"
+        // src="/src/assets/images/logo.png"
+        src={logo}
         height="100%"
         alt="logo"
       />
